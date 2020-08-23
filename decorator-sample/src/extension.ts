5bce0e368a2eed73f7b079d8973064ea8ef5fda2
Masayuki Ono <mono0926@gmail.com>
2020-08-23 15:16:56 +0900

    Change color

--- decorator-sample/src/extension.ts
+++ decorator-sample/src/extension.ts
@@ -1,89 +1,93 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 // this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
-	console.log('decorator sample is activated');
+  console.log('decorator sample is activated')
 
-	let timeout: NodeJS.Timer | undefined = undefined;
+  let timeout: NodeJS.Timer | undefined = undefined
 
-	// create a decorator type that we use to decorate small numbers
-	const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
-		// borderWidth: '1px',
-		// borderStyle: 'solid',
-		// overviewRulerColor: 'blue',
-		// overviewRulerLane: vscode.OverviewRulerLane.Right,
-		// light: {
-		// 	// this color will be used in light color themes
-		// 	borderColor: 'darkblue',
-		// },
-		// dark: {
-		// 	// this color will be used in dark color themes
-		// 	borderColor: 'lightblue',
-		// },
-		after: {
-			color: 'rgba(255, 0, 0, 1.0)',
-			margin: '2px',
-		},
-		rangeBehavior: vscode.DecorationRangeBehavior.ClosedOpen,
-	});
+  // create a decorator type that we use to decorate small numbers
+  const smallNumberDecorationType = vscode.window.createTextEditorDecorationType(
+    {
+      // borderWidth: '1px',
+      // borderStyle: 'solid',
+      // overviewRulerColor: 'blue',
+      // overviewRulerLane: vscode.OverviewRulerLane.Right,
+      // light: {
+      // 	// this color will be used in light color themes
+      // 	borderColor: 'darkblue',
+      // },
+      // dark: {
+      // 	// this color will be used in dark color themes
+      // 	borderColor: 'lightblue',
+      // },
+      after: {
+        color: 'green',
+        margin: '2px',
+      },
+      rangeBehavior: vscode.DecorationRangeBehavior.ClosedOpen,
+    }
+  )
 
-	let activeEditor = vscode.window.activeTextEditor;
+  let activeEditor = vscode.window.activeTextEditor
 
-	function updateDecorations() {
-		if (!activeEditor) {
-			return;
-		}
-		4;
-		const regEx = /.+/g;
-		const text = activeEditor.document.getText();
-		const smallNumbers: vscode.DecorationOptions[] = [];
-		let match;
-		while ((match = regEx.exec(text))) {
-			const pos = activeEditor.document.positionAt(match.index + match[0].length);
-			const decoration = <vscode.DecorationOptions>{
-				range: new vscode.Range(pos, pos),
-				hoverMessage: 'Number **' + match[0] + '**',
-				renderOptions: {
-					after: {
-						contentText: '  🐶🐶🐶🐶Dog',
-					},
-				},
-			};
-			smallNumbers.push(decoration);
-		}
-		activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
-	}
+  function updateDecorations() {
+    if (!activeEditor) {
+      return
+    }
+    4
+    const regEx = /.+/g
+    const text = activeEditor.document.getText()
+    const smallNumbers: vscode.DecorationOptions[] = []
+    let match
+    while ((match = regEx.exec(text))) {
+      const pos = activeEditor.document.positionAt(
+        match.index + match[0].length
+      )
+      const decoration = <vscode.DecorationOptions>{
+        range: new vscode.Range(pos, pos),
+        hoverMessage: 'Number **' + match[0] + '**',
+        renderOptions: {
+          after: {
+            contentText: '  🐶🐶🐶🐶Dog',
+          },
+        },
+      }
+      smallNumbers.push(decoration)
+    }
+    activeEditor.setDecorations(smallNumberDecorationType, smallNumbers)
+  }
 
-	function triggerUpdateDecorations() {
-		if (timeout) {
-			clearTimeout(timeout);
-			timeout = undefined;
-		}
-		timeout = setTimeout(updateDecorations, 500);
-	}
+  function triggerUpdateDecorations() {
+    if (timeout) {
+      clearTimeout(timeout)
+      timeout = undefined
+    }
+    timeout = setTimeout(updateDecorations, 500)
+  }
 
-	if (activeEditor) {
-		triggerUpdateDecorations();
-	}
+  if (activeEditor) {
+    triggerUpdateDecorations()
+  }
 
-	vscode.window.onDidChangeActiveTextEditor(
-		(editor) => {
-			activeEditor = editor;
-			if (editor) {
-				triggerUpdateDecorations();
-			}
-		},
-		null,
-		context.subscriptions
-	);
+  vscode.window.onDidChangeActiveTextEditor(
+    (editor) => {
+      activeEditor = editor
+      if (editor) {
+        triggerUpdateDecorations()
+      }
+    },
+    null,
+    context.subscriptions
+  )
 
-	vscode.workspace.onDidChangeTextDocument(
-		(event) => {
-			if (activeEditor && event.document === activeEditor.document) {
-				triggerUpdateDecorations();
-			}
-		},
-		null,
-		context.subscriptions
-	);
+  vscode.workspace.onDidChangeTextDocument(
+    (event) => {
+      if (activeEditor && event.document === activeEditor.document) {
+        triggerUpdateDecorations()
+      }
+    },
+    null,
+    context.subscriptions
+  )
 }
